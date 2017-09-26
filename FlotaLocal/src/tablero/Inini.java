0982529a4f90f4c76fff19756d95f278a16d94af
MariusMiau <MariusMiau@HACKVILLE-CENTER>
2017-09-27 00:54:08 +0200

    FlotaLocal

---
+++ FlotaLocal/src/tablero/Inini.java
@@ -0,0 +1,10 @@
+package tablero;
+
+public class Inini {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//dfbgfdbrfobsbnotrnbrnoritgnboifgbrtfgfgbfgfbgbgbvbcvbfgbfgbbgbgfbgbfgbfgbgcbsgfbfzxbg 
+	}
+
+}
